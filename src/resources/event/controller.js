1a73a28b03c592bb79ad2cc7f517f82e13edae2b
j_cheney <joshcheney125@gmail.com>
2021-10-26 10:59:58 +0100

    getEventById finished

--- src/resources/event/controller.js
+++ src/resources/event/controller.js
@@ -1,1 +1,21 @@
 const prisma = require("../../utils/database");
+
+function getEventById(req, res) {
+    console.log("working")
+    prisma.event.findUnique({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
+        .then((result) => {
+            console.log(result)
+            res.json({ data: result })
+        })
+        .catch(error => {
+            console.error(error)
+            res.status(500).json(error)
+        })
+}
+
+
+module.exports = { getEventById }
--- src/resources/event/router.js
+++ src/resources/event/router.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+const { getEventById } = require("./controller");
+
 const router = express.Router();
+
+router.get("/:id", getEventById)
 
 module.exports = router;
