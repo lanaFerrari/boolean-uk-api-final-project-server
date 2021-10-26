38e0cc9593f1ca1969099f4c1a3ab249fd76979d
Lana Ferrari <ferrarirangel@gmail.com>
2021-10-26 15:01:36 +0100

    deleteCostumer

--- src/resources/customer/controller.js
+++ src/resources/customer/controller.js
@@ -19,14 +19,21 @@ const createCustomer = async (req, res) =>{
 };
 
 const deleteById = async (req, res) =>{
+try {
     const reqId = req.params.id;
+    console.log("connected", reqId);
 
-    const result = await prisma.customer.deleteMany({
+    const deleteCustomer = await prisma.customer.delete({
         where:{
             id: parseInt(reqId),
         },
     })
-    res.json({ data: result });  
+
+    res.json({ data: deleteCustomer }); 
+} catch (error) {
+    console.error({ error: error.message });
+    res.status(500).json({ error: error.message });
+} 
 }
 
 module.exports ={
