f3ce1eb715bb33a313396f5d571508d8e66d27dc
Lana Ferrari <ferrarirangel@gmail.com>
2021-10-26 11:22:28 +0100

    venue put request

--- src/resources/venue/controller.js
+++ src/resources/venue/controller.js
@@ -1,1 +1,28 @@
 const prisma = require("../../utils/database");
+
+const editVenueWithId = async (req, res) =>{
+    const venueId = req.params.id;
+    const { buildingName, city, street, postCode } = req.body;
+    
+    try {
+            const result = await prisma.venue.update({
+        where:{
+            id: parseInt(venueId),
+        },
+        data:{
+            buildingName,
+            city,
+            street,
+            postCode
+        },
+    })
+    res.json({ data: result })
+    } catch (error) {
+        console.error({ error: error.message });
+        res.status(500).json({ error: error.message }); 
+    }
+};
+
+module.exports={
+    editVenueWithId
+};
