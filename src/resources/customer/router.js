21aa9a5f1da8b51f5eae013be9ebea3756c58757
Lana Ferrari <ferrarirangel@gmail.com>
2021-10-26 10:34:09 +0100

    createCostumer-check

--- src/resources/customer/router.js
+++ src/resources/customer/router.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+const { createCustomer } = require("./controller");
+
 const router = express.Router();
+
+router.post("/", createCustomer);
 
 module.exports = router;
